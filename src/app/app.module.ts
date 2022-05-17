c019e9c64d2977583af1445afab10d9b08016a2e
Surekha Shelake <shelakesurekha@gmail.com>
2022-05-17 09:50:14 +0530

    added components header, fotter user, list-user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { FoooterComponent } from './foooter/foooter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    ListUserComponent,
+    MainContainerComponent,
+    FoooterComponent
   ],
   imports: [
     BrowserModule
